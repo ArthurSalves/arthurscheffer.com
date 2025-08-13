d358da58d480c019da62f2f143a57eedbc8be29d
ArthurSalves <arthurschefferalves@hotmail.com>
2025-08-13 14:11:48 -0300

    fix: seo and accessibility

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 [ ! -d '/tmp/cache' ] && mkdir -p /tmp/cache
 
 exec node server.js
+
+node -e "process.on('uncaughtException', console.error); process.on('unhandledRejection', console.error); require('./server.js')"
